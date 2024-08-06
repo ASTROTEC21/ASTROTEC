d133335910f1a09d1b1ca73c0c31aa3fc56c0fd2
ASTROTEC21 a <edutecastro21@gmail.com>
2024-08-06 20:30:37 +0000

    Finalização das galáxias e linkagem de tudo

---
+++ pages/Planeta marte/marte.js
@@ -0,0 +1,38 @@
+const nav = document.querySelector("header nav.menu")
+const body = document.querySelector("body")
+const botaoAbrirMenu = document.querySelector("header button.menu")
+botaoAbrirMenu.addEventListener("click", menuAbrir)
+
+function menuAbrir(){
+    nav.classList.add("abrir")
+    body.classList.add("escurecer")
+}
+
+const botaoFecharMenu = document.querySelector("header nav.menu button")
+botaoFecharMenu.addEventListener("click", menuFechar)
+
+function menuFechar(){
+    nav.classList.remove("abrir")
+    body.classList.remove("escurecer")
+}
+
+const botaoLogo = document.querySelector("header button")
+botaoLogo.addEventListener("click", paginaInicial)
+
+function paginaInicial(){
+    window.location.href = "../../index.html"
+}
+
+const botaoGalaxias = document.querySelector("header nav ul #galaxias")
+botaoGalaxias.addEventListener("click", galaxiasIr)
+
+function galaxiasIr(){
+    window.location.href = "../galaxias/galaxia.html"
+}
+
+const botaoSatelite = document.querySelector("header nav ul #satelites")
+botaoSatelite.addEventListener("click", satelitesIr)
+
+function satelitesIr(){
+    window.location.href = "../satelites/satelite.html"
+}
